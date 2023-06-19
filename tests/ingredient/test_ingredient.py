9f0f4b0b4b4670247a2f7adfe16ce43fa164bddf
willianAD <willian.afonso013@gmail.com>
2023-06-19 14:47:53 -0300

    test: test_ingredient - Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1
-def test_ingredient():
-    pass
+def test_ingredient(self):
+    ingredient1 = Ingredient('queijo mussarela')
+    ingredient2 = Ingredient('queijo mussarela')
+
+    hash1 = hash(ingredient1)
+    hash2 = hash(ingredient2)
+
+    self.assertEqual(hash1, hash2)
+
+
+def test_ingretient_not_equal(self):
+    ingredient1 = Ingredient('queijo mussarela')
+    ingredient2 = Ingredient('bacon')
+
+    hash1 = hash(ingredient1)
+    hash2 = hash(ingredient2)
+
+    self.assertNotEqual(hash1, hash2)
