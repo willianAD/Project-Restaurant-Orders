558fcd2c2d54d206b30161a5d0a32f862b29b899
willianAD <willian.afonso013@gmail.com>
2023-06-19 17:51:58 -0300

    test: test_ingredient - Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,29 +1,25 @@
 from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-def test_ingredient(self):
-    ingredient1 = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("queijo mussarela")
-
-    hash1 = hash(ingredient1)
-    hash2 = hash(ingredient2)
-
-    assert hash1 == hash2
-
+def test_ingredient():
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("bacon")
 
-    hash1 = hash(ingredient1)
-    hash2 = hash(ingredient2)
+    assert ingredient1 == Ingredient("queijo mussarela")
+    assert ingredient1 != ingredient2
 
-    assert hash1 != hash2
+    assert ingredient1.name == "queijo mussarela"
+    assert ingredient2.name != "queijo mussarela"
 
-    ingredient = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("bacon")
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
+    assert repr(ingredient2) != "Ingredient('queijo mussarela')"
+    assert hash(ingredient1) == hash("queijo mussarela")
+    assert hash(ingredient2) != hash("queijo mussarela")
+
     expected_restrictions = {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,
     }
 
-    assert ingredient.restrictions == expected_restrictions
-    assert ingredient2 != expected_restrictions
+    assert ingredient1.restrictions == expected_restrictions
+    assert ingredient2.restrictions != expected_restrictions
