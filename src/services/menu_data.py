519d98ff2f23091b08daabe61e5ce40e0629a8b5
willianAD <willian.afonso013@gmail.com>
2023-06-19 17:13:37 -0300

    test: test_ingredient - Requisito 1

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,31 @@
-# Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = set()
+
+    def load_menu_data(self):
+        with open(self.source_path, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                dish_name = row[0]
+                price = float(row[1])
+                ingredient_name = row[2]
+                quantity = int(row[3])
+
+                plate = next(
+                    (d for d in self.dishes if d.name == dish_name),
+                    None,
+                )
+                if plate is None:
+                    dish = Dish(dish_name, price)
+                    self.dishes.add(plate)
+                    ingredient = Ingredient(ingredient_name)
+
+                    dish.add_ingredient_dependency(ingredient, quantity)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,4 +1,4 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 def test_ingredient(self):
@@ -8,14 +8,22 @@ def test_ingredient(self):
     hash1 = hash(ingredient1)
     hash2 = hash(ingredient2)
 
-    self.assertEqual(hash1, hash2)
+    assert hash1 == hash2
 
-
-def test_ingretient_not_equal(self):
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("bacon")
 
     hash1 = hash(ingredient1)
     hash2 = hash(ingredient2)
 
-    self.assertNotEqual(hash1, hash2)
+    assert hash1 != hash2
+
+    ingredient = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("bacon")
+    expected_restrictions = {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert ingredient.restrictions == expected_restrictions
+    assert ingredient2 != expected_restrictions
